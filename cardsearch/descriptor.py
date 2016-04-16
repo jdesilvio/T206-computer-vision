e46566a4ab1f3be8fd1427a008d5a35a1b4a436f
John DeSilvio <john.desilvio@yahoo.com>
2016-04-16 11:44:58 -0400

    Removed some debugging lines from descriptor.py

--- cardsearch/descriptor.py
+++ cardsearch/descriptor.py
@@ -3,6 +3,7 @@
 import numpy as np
 import cv2
 
+
 class CardDescriptor:
     def __init__(self, kpMethod="SIFT", descMethod="SIFT"):
         self.kpMethod = kpMethod
@@ -11,15 +12,13 @@ class CardDescriptor:
     # Extract key points and descriptors for an image
     def describe(self, image):
         sift = cv2.xfeatures2d.SIFT_create()
-	#detector = cv2.FeatureDetector_create(self.kpMethod)
+        #detector = cv2.FeatureDetector_create(self.kpMethod)
         #kps = detector.detect(image)
 
         #extractor = cv2.DescriptorExtractor_create(self.descMethod)
         #(kps, descs) = extractor.compute(image, kps)
-        
-	(kps, descs) = sift.detectAndCompute(image, None)
 
-	kps = np.float32([kp.pt for kp in kps])
-        print str(image)
-        print (kps, descs)
-	return (kps, descs)
+        (kps, descs) = sift.detectAndCompute(image, None)
+
+        kps = np.float32([kp.pt for kp in kps])
+        return (kps, descs)
