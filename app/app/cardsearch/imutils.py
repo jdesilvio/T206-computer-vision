3321d300ecefb721ee22c6a04d219543c1c50378
John DeSilvio <john.desilvio@yahoo.com>
2016-09-02 20:12:38 -0400

    Added imutils.py for image utility functions, added function for resizing an image.

---
+++ app/app/cardsearch/imutils.py
@@ -0,0 +1,22 @@
+import cv2
+
+
+def resize(image, width=None, height=None, inter=cv2.INNER_AREA):
+    """Resize an image while keeping the same aspect ratio."""
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    return resized
